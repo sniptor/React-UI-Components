3d625d366926ab0c1b2e313caeef3d903072b0c1
Bhaskar Sawant <bhaskar.csawant417@gmail.com>
2023-05-30 20:54:27 +0530

    added more components

---
+++ FlamingoBtn1/FlamingoBtn1.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from  './FlamingoBtn1.module.css'
+
+function FlamingoBtn1() {
+  return <button className={styles.ButtonContainer} role='button'>Label</button>;
+}
+
+export default FlamingoBtn1
---
+++ FlamingoBtn2/FlamingoBtn2.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./FlamingoBtn2.module.css";
+
+function FlamingoBtn2() {
+  return (
+    <button className={styles.ButtonContainer} role="button">
+      Label
+    </button>
+  );
+}
+
+export default FlamingoBtn2;
